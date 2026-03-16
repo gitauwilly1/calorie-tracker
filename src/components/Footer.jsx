03f89bf37c2d0aea2d7aa9d682bef6b13adc35f9
gitauwilly1 <gitauwilly254@gmail.com>
2026-03-16 10:25:19 +0300

    Create reusable Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer className="p-6 text-center text-slate-400 text-xs uppercase tracking-tighter">© 2026 Vita-Track Systems</footer>;
+  return (
+    <footer className="p-6 text-center text-slate-500 text-xs uppercase tracking-tighter mt-auto">
+      © 2026 Vita-Track Systems
+    </footer>
+  );
 }
